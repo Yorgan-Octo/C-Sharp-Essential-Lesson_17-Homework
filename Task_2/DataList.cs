a166ec2ce449fbbe2ceafcdc9d2dec4ab804a83c
Artem Priadchenko <yorgan.info@gmail.com>
2023-06-06 00:02:04 +0300

    Добавьте файлы проекта.

---
+++ Task_2/DataList.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.ConstrainedExecution;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_2
+{
+
+    public enum BrandCarsEnum
+    {
+        Acura,
+        Audi,
+        Bentley,
+        BMW,
+        Brilliance,
+        Bugatti,
+        Ford
+    }
+
+
+    public class Car
+    {
+        public string Model { get; set; }
+        public BrandCarsEnum CarBrand { get; set; }
+        public DateTime GraduationYear { get; set; }
+        public ConsoleColor Color { get; set; }
+
+        public void DrawInfoCar()
+        {
+            Console.WriteLine(new String('-', 50));
+            Console.ForegroundColor = ConsoleColor.DarkBlue;
+            Console.WriteLine($"Автомобіль: {CarBrand} {Model}");
+            Console.WriteLine($"Рік випуску: {GraduationYear.Year}");
+            Console.WriteLine($"Колір: {Color}");
+            Console.ResetColor();
+
+        }
+
+    }
+
+    public class Client
+    {
+        public string Name { get; set; }
+        public string NamberPhone { get; set; }
+        public string ModelCars { get; set; }
+    }
+
+
+}
---
+++ Task_2/Program.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_2
+{
+    public class Program
+    {
+        static void Main()
+        {
+
+            List<Car> cars = new List<Car>()
+            {
+                new Car()
+                {
+                    Model = "GT500",
+                    CarBrand = BrandCarsEnum.Ford,
+                    GraduationYear = DateTime.Parse("2/4/2022"),
+                    Color = ConsoleColor.DarkRed,
+                },
+
+                new Car()
+                {
+                    Model = "QWJ17",
+                    CarBrand = BrandCarsEnum.Bentley,
+                    GraduationYear = DateTime.Parse("12/10/1983"),
+                    Color = ConsoleColor.DarkBlue,
+                },
+
+                new Car()
+                {
+                    Model = "LS789",
+                    CarBrand = BrandCarsEnum.Acura,
+                    GraduationYear = DateTime.Parse("2/2/2002"),
+                    Color = ConsoleColor.Yellow,
+                },
+
+                new Car()
+                {
+                    Model = "BT2311",
+                    CarBrand = BrandCarsEnum.Bugatti,
+                    GraduationYear = DateTime.Parse("3/7/1964"),
+                    Color = ConsoleColor.Blue,
+                },
+
+                new Car()
+                {
+                    Model = "Sirius",
+                    CarBrand = BrandCarsEnum.Bentley,
+                    GraduationYear = DateTime.Parse("4/4/2013"),
+                    Color = ConsoleColor.Magenta,
+                },
+            };
+
+            List<Client> clients = new List<Client>()
+            {
+
+                new Client()
+                {
+                    Name = "Dima",
+                    NamberPhone = "+380971324589",
+                    ModelCars = "GT500"
+                },
+
+                new Client()
+                {
+                    Name = "Alisa",
+                    NamberPhone = "+380974569834",
+                    ModelCars = "QWJ17"
+                },
+
+                new Client()
+                {
+                    Name = "Clara",
+                    NamberPhone = "+380971256723",
+                    ModelCars = "LS789"
+                },
+
+                new Client()
+                {
+                    Name = "Sara",
+                    NamberPhone = "+380973457689",
+                    ModelCars = "BT2311"
+                },
+
+                new Client()
+                {
+                    Name = "Liza",
+                    NamberPhone = "+380964356767",
+                    ModelCars = "Sirius"
+                },
+
+            };
+
+
+            var data =
+                from car in cars
+                join client in clients
+                on car.Model equals client.ModelCars
+                let clientInfo = $"Покупатель: {client.Name}, Номер {client.NamberPhone}"
+                orderby car.Model descending
+                select new
+                {
+                    Client = clientInfo,
+                    Car = car,
+                };
+
+
+            foreach (var item in data)
+            {
+                Console.WriteLine(new String('=', 50));
+                Console.WriteLine(item.Client);
+                item.Car.DrawInfoCar();
+                Console.WriteLine(new String('=', 50));
+                Console.WriteLine();
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
